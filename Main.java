fe47a21fc43d167157db676452f3bc94c0cb6ea8
3Aintse <up617687@myport.ac.uk>
2012-11-24 15:35:29 +0000

    first summit of coursework 11/24/2012 3:35

---
+++ Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intse;
+
+/**
+ *
+ * @author Craig
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        GUI gui = new GUI();
+        gui.main(null);
+    }
+}
